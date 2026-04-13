46bd7dd05257767db47d5a0c73a4dbc750f7e002
nour <nouurmohaamed@icloud.com>
2026-04-13 20:41:52 +0200

    a function that creates the table

---
+++ table_widget.py
@@ -0,0 +1,55 @@
+from PyQt6.QtWidgets import QApplication,QMainWindow,QTableWidget,QTableWidgetItem,QVBoxLayout,QWidget,QHeaderView
+def createTable(self):
+        table=QTableWidget(10,4)  #assume the user choose to first enter 3 processes it will later be replaced by n process where n is input from user
+        table.setEditTriggers(QTableWidget.EditTrigger.NoEditTriggers) #disable editting the table
+        table.setSelectionMode(QTableWidget.SelectionMode.NoSelection) #disable selecting cells
+        table.horizontalHeader().setSectionResizeMode(
+        QHeaderView.ResizeMode.Stretch
+        ) #resize the cells width
+        table.verticalHeader().setDefaultSectionSize(30) #resize the cells height
+        table.verticalHeader().setVisible(False) #hide the indexing of rows
+        table.setAlternatingRowColors(True) #make the cells color alternating
+        table.setStyleSheet("""
+        *{
+             color:black;
+            font-weight: 550;
+            }
+            QTableWidget {
+            background-color: white;
+            font-size: 15px;
+            border: none;
+        }
+
+            QTableWidget::item {
+            padding: 5px;
+        }
+
+            QTableWidget::item:selected {
+            background-color: #3498db;
+            color: white;
+        }
+
+        QHeaderView::section {
+        background-color: #3498db;
+        color: white;
+        padding: 6px;
+        font-weight: bold;
+        font-size:15px;
+       border: 0.5px solid #2980b9;
+        }
+
+        QTableWidget {
+        alternate-background-color: #f5f5f5;
+        }
+        QTableWidget::item:hover {
+        background-color: #d6ecff;
+        }
+
+""")
+        #creating the initial processes
+        table.setHorizontalHeaderLabels(["process id","arrival time","burst time","remaining burst time"])
+        table.setItem(0,0,QTableWidgetItem("a")) #those are dummy variables will be changed later
+        table.setItem(0,1,QTableWidgetItem("b"))
+        table.setItem(0,2,QTableWidgetItem("d"))
+        table.setItem(0,3,QTableWidgetItem("e"))
+        return table
