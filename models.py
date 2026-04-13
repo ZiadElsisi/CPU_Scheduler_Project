0e7609abecaed51b2e4f1a76e1f0cb4ab96bc393
Mona <monawalid166@gmail.com>
2026-04-13 17:13:05 +0200

    Create Process Function

---
+++ models.py
@@ -0,0 +1,25 @@
+def create_process(pid, arrival, burst):
+
+    process = {
+        "id": pid,
+        "arrival": arrival,
+        "burst": burst,
+        "remaining": burst  
+    }
+    return process
+
+
+
+
+if __name__ == "__main__":
+
+  p1 = create_process("P1", 0, 5)
+  p2 = create_process("P2", 2, 8)
+  p3 = create_process("P3", 4, 3)
+  
+  processes = [p1, p2, p3]
+
+  print("===== Created Processes =====")
+  
+  for p in processes:
+        print(p)
